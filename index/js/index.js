8841a701c066dcab100c2f6c84492c7dd9bd5a49
matthe68 <matthewbchristensen@comcast.net>
2022-01-08 13:58:04 -0700

    fixed name issue in json

--- index/js/index.js
+++ index/js/index.js
@@ -6,8 +6,8 @@ window.onload = async function() {
   await fetch("https://matthe68.github.io/WDD330_Portfolio/index/json/dynamic-table-of-contents.json")
     .then(response => response.json())
     .then(function(json) {
-      for (let i in json) {
-        console.log(i);
+      for (let i in json["dynamic-table-of-contents"]) {
+        console.log(json["dynamic-table-of-contents"][i]);
       }
       console.log(json)
     });
