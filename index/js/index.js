e3323e1efd41ac5d464174f818f89d33db9b68aa
matthe68 <matthewbchristensen@comcast.net>
2022-01-08 14:08:19 -0700

    testing creating dynamic table of contents

--- index/js/index.js
+++ index/js/index.js
@@ -6,8 +6,19 @@ window.onload = async function() {
   await fetch("https://matthe68.github.io/WDD330_Portfolio/index/json/dynamic-table-of-contents.json")
     .then(response => response.json())
     .then(function(json) {
-      for (let i in json["dynamic-table-of-contents"]) {
-        console.log(json["dynamic-table-of-contents"][i]);
+      for (let i in json.dynamicTableOfContents) {
+        let dynamicTable = document.querySelector('#dynamic-table-of-contents');
+        let li = document.createElement('li');
+        let label = document.createElement('span');
+        let a = document.createElement('a');
+
+        label = json.dynamicTableOfContents[i].label;
+        let link = json.dynamicTableOfContents[i].url;
+        a.href = link;
+        a.innerHTML = link;
+        li.appendChild(label);
+        li.appendChild(a);
+        dynamicTable.appendChild(li);
       }
       console.log(json)
     });
