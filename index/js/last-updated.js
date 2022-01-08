1012a8cae43137452e0be570b6f95d4aefbed591
matthe68 <matthewbchristensen@comcast.net>
2022-01-08 15:42:40 -0700

    forgot to save my files first

--- index/js/last-updated.js
+++ index/js/last-updated.js
@@ -2,26 +2,4 @@ window.onload = async function() {
   const time = "Last Updated: " + String(document.lastModified);
   var lastUpdated = document.querySelector("#last-updated");
   lastUpdated.innerHTML = time;
-
-  await fetch("https://matthe68.github.io/WDD330_Portfolio/index/json/dynamic-table-of-contents.json")
-    .then(response => response.json())
-    .then(function(json) {
-      for (let i in json.dynamicTableOfContents) {
-        console.log("update: 2:07");
-        let dynamicTable = document.querySelector('#assignment-ordered-list');
-        let li = document.createElement('li');
-        let label = document.createElement('span');
-        let a = document.createElement('a');
-
-        label.innerHTML = json.dynamicTableOfContents[i].label;
-        let link = json.dynamicTableOfContents[i].url;
-        let linkText = json.dynamicTableOfContents[i].urlText;
-        a.href = link;
-        a.innerHTML = String(linkText);
-        li.appendChild(label);
-        li.appendChild(a);
-        dynamicTable.appendChild(li);
-      }
-      console.log(json)
-    });
 }
