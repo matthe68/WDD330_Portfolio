e48436b4b525c801a83d322ebfe8f298c7f9b887
matthe68 <matthewbchristensen@comcast.net>
2022-01-08 16:17:50 -0700

    fixed problem in my js

--- index/js/last-updated.js
+++ index/js/last-updated.js
@@ -1,6 +1,4 @@
-window.onload = async function() {
-  console.log("inside last updated!");
-  const time = "Last Updated: " + String(document.lastModified);
-  var lastUpdated = document.querySelector("#last-updated");
-  lastUpdated.innerHTML = time;
-}
+console.log("inside last updated!");
+const time = "Last Updated: " + String(document.lastModified);
+var lastUpdated = document.querySelector("#last-updated");
+lastUpdated.innerHTML = time;
