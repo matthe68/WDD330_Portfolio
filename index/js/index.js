f1f4de124c518e7987d78bb477b81fc16d1e1148
matthe68 <matthewbchristensen@comcast.net>
2022-01-08 13:54:37 -0700

    dynamic table of contents json is working

--- index/js/index.js
+++ index/js/index.js
@@ -3,7 +3,12 @@ window.onload = async function() {
   var lastUpdated = document.querySelector("#last-updated");
   lastUpdated.innerHTML = time;
 
-  await fetch("../json/dynamic-table-of-contents.json")
+  await fetch("https://matthe68.github.io/WDD330_Portfolio/index/json/dynamic-table-of-contents.json")
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(function(json) {
+      for (let i in json) {
+        console.log(i);
+      }
+      console.log(json)
+    });
 }
