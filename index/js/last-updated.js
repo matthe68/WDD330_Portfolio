e1bc447c2697c2c21feb9bdd93d8ca93294a2058
matthe68 <matthewbchristensen@comcast.net>
2022-01-08 16:10:58 -0700

    fixed html validation error

--- index/js/last-updated.js
+++ index/js/last-updated.js
@@ -1,4 +1,5 @@
 window.onload = async function() {
+  console.log("inside last updated!");
   const time = "Last Updated: " + String(document.lastModified);
   var lastUpdated = document.querySelector("#last-updated");
   lastUpdated.innerHTML = time;
