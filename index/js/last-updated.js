19fd8a7ebf2168541df17d0596c50510468cbda4
matthe68 <matthewbchristensen@comcast.net>
2022-01-08 17:05:41 -0700

    finished the story editor and the notes from the activity and reading

--- index/js/last-updated.js
+++ index/js/last-updated.js
@@ -1,4 +1,3 @@
-console.log("inside last updated!");
 const time = "Last Updated: " + String(document.lastModified);
 var lastUpdated = document.querySelector("#last-updated");
 lastUpdated.innerHTML = time;
