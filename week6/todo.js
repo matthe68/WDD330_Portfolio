bedc8cdc861fbccd98f11cf5db10d4c2d69e9743
Matthew Christensen <matthewbchristensen@comcast.net>
2022-02-12 19:45:06 -0700

    playing with toggle functions

--- week6/todo.js
+++ week6/todo.js
@@ -1,6 +1,15 @@
 import { ListObject } from "./listObject.js";
 import { ListItem } from "./listObject.js";
 
+function visibilityToggle() {
+  if (this.classList.contains('filter-toggle')) {
+    let visibilityButtons = document.querySelector('.filter-toggle');
+    visibilityButtons.removeClass('active');
+    this.classList.add('active');
+  }
+  
+}
+
 // function createListItem()
 let listObject = new ListObject();
 let todoHolder = document.querySelector("#todo-list");
@@ -8,12 +17,22 @@ let listFooter = document.createElement('div');
 let numTasks = document.createElement('span');
 numTasks.innerHTML = `${listObject.numberOfTasks()} tasks left`;
 let activeStatus = document.createElement('span');
+
 let buttonAll = document.createElement('button');
 buttonAll.innerHTML = "All";
+buttonAll.classList.add('filterToggle');
+
 let buttonActive = document.createElement('button');
 buttonActive.innerHTML = "Active";
+buttonActive.classList.add('filter-toggle');
+
 let buttonCompleted = document.createElement('button');
 buttonCompleted.innerHTML = "Completed";
+buttonCompleted.classList.add('filter-toggle');
+
+buttonAll.addEventListener('click', visibilityToggle());
+buttonActive.addEventListener('click', visibilityToggle());
+buttonActive.classList.add('filter-toggle');
 
 activeStatus.appendChild(buttonAll);
 activeStatus.appendChild(buttonActive);
